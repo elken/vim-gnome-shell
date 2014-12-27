ac459fe3f8a719fddc1595d5008d3ee3c2416d85
Ellis Kenyo <elken.tdos@gmail.com>
2014-12-27 22:02:10 +0000

    Cleanup mostly
    - Re-arranged functions
    - Added sane oop checks
    - Added basic panel code

--- autoload/vim_gnome_shell.py
+++ autoload/vim_gnome_shell.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
-import string
 import json
-import sys
 import os
+import vim
 
 from gi.repository import Gio
 
+
 class GnomeShell(object):
 
-    def __init__(self):
+    def init(self):
         try:
             self.json = open('metadata.json')
-        except IOError, e:
-            return 
+        except IOError:
+            return
 
         self.data = json.load(self.json)
         self.json.close()
@@ -22,7 +22,7 @@ class GnomeShell(object):
         self.desc = self.data['description']
         try:
             self.url = self.data['url']
-        except KeyError, e:
+        except KeyError:
             print "No URL in metadata.json"
             self.url = "none"
 
@@ -49,7 +49,10 @@ class GnomeShell(object):
             return 0
 
     def hasPrefs(self):
-        return os.path.exists(os.path.join(os.environ['HOME'],".local/share/gnome-shell/extensions/",self.uuid,"prefs.js"))
+        return os.path.exists(
+            os.path.join(os.environ['HOME'],
+                         ".local/share/gnome-shell/extensions/",
+                         self.uuid, "prefs.js"))
 
     def openPrefs(self):
         if self.hasPrefs():
@@ -59,70 +62,68 @@ class GnomeShell(object):
 
     def enableExtension(self):
         if self.getState() == 1:
-            print self.uuid + " is already enabled." 
+            print self.uuid + " is already enabled."
             return
 
         self.extensions.append(self.uuid)
         self.settings.set_strv('enabled-extensions', self.extensions)
-        print self.uuid + " is now enabled." 
+        print self.uuid + " is now enabled."
 
     def disableExtension(self):
         if self.getState() == 0:
-            print self.uuid + " is not enabled or installed." 
+            print self.uuid + " is not enabled or installed."
             return
 
         while self.uuid in self.extensions:
             self.extensions.remove(self.uuid)
 
         self.settings.set_strv('enabled-extensions', self.extensions)
-        print self.uuid + " is now disabled." 
+        print self.uuid + " is now disabled."
 
-g
+g = GnomeShell()
 
-def reset():
-    vim.command("let g:vim_gnome_shell_uuid = ''")
-    vim.command("let g:vim_gnome_shell_name = ''")
-    vim.command("let g:vim_gnome_shell_cwd = ''")
-    vim.command("let g:vim_gnome_shell_state = ''")
 
 def initVGS():
-    reset()
-    g = GnomeShell()
+    g.init()
     try:
         vim.command("let g:vim_gnome_shell_uuid = '%s'" % g.getUUID())
         vim.command("let g:vim_gnome_shell_name = '%s'" % g.getName())
-    except AttributeError, e:
-        print "g is not defined, unable to find metadata.json"
+    except AttributeError:
+        print "Object not loaded, can't init"
         vim.command("let g:vim_gnome_shell_loaded = 0")
     else:
         vim.command("let g:vim_gnome_shell_loaded = 1")
+
 
 def VGSEnable():
     try:
         g.enableExtension()
-    except AttributeError, e:
-        print "g is not defined, unable to find metadata.json"
+    except AttributeError:
+        print "Object not loaded, can't enable"
         vim.command("let g:vim_gnome_shell_loaded = 0")
     else:
         vim.command("let g:vim_gnome_shell_loaded = 1")
+
 
 def VGSDisable():
     try:
         g.disableExtension()
-    except AttributeError, e:
-        print "g is not defined, unable to find metadata.json"
+    except AttributeError:
+        print "Object not loaded, can't disable"
         vim.command("let g:vim_gnome_shell_loaded = 0")
     else:
         vim.command("let g:vim_gnome_shell_loaded = 1")
 
+
 def VGSUpdateState():
     try:
         vim.command("let g:vim_gnome_shell_state = %d" % g.getState())
-    except AttributeError, e:
-        print "g is not defined, unable to find metadata.json"
+    except AttributeError:
+        print "Object not loaded, can't update state"
         vim.command("let g:vim_gnome_shell_loaded = 0")
     else:
         vim.command("let g:vim_gnome_shell_loaded = 1")
+
 
 def VGSIsExtensionDir():
     try:
@@ -134,20 +135,23 @@ def VGSIsExtensionDir():
             return 1
         else:
             return 0
-    except AttributeError, e:
-        print "g is not defined, unable to find metadata.json"
+    except AttributeError:
+        print "Object not loaded, can't check if dir"
         vim.command("let g:vim_gnome_shell_loaded = 0")
     else:
         vim.command("let g:vim_gnome_shell_loaded = 1")
 
+
 def normal(str):
     vim.command("normal "+str)
+
 
 def insert(arg):
     for i in arg:
         normal("o")
         vim.current.line = i
-    
+
+
 def append(arg):
     for i in arg:
         vim.current.line = vim.current.line + i + " "
--- autoload/vim_gnome_shell.vim
+++ autoload/vim_gnome_shell.vim
@@ -1,11 +1,14 @@
+" Python check {{{
 if !has('python')
     echom "Python not installed, exiting"
     finish
 endif
-
+" }}}
+" Init {{{
 let s:plugin_path = escape(expand('<sfile>:p:h'), '\')
 execute 'pyfile ' . s:plugin_path . '/vim_gnome_shell.py'
-
+" }}}
+" Variables {{{
 if !exists("g:vim_gnome_shell_uuid")
     let g:vim_gnome_shell_uuid=""
 endif
@@ -26,47 +29,96 @@ if !exists("g:vim_gnome_shell_loaded")
     let g:vim_gnome_shell_loaded=1
 endif
 
+if !exists("g:vim_gnome_shell_width")
+    let g:vim_gnome_shell_width = 80
+endif
+
+if !exists("g:vim_gnome_shell_vertical")
+    let g:vim_gnome_shell_vertical = 0
+endif
+" }}}
+" s:GetCWD() {{{
 function! s:GetCWD()
     return system('basename `pwd`')
 endfunction
-
+" }}}
+" s:SetCWD() {{{
 function! s:SetCWD()
     let g:vim_gnome_shell_cwd = s:GetCWD()
 endfunction
-
-function! vim_gnome_shell#VGSAuCheck()
+" }}}
+" vim_gnome_shell#AuCheck() {{{
+function! vim_gnome_shell#AuCheck()
     python VGSIsExtensionDir()
 endfunction
-
-function! vim_gnome_shell#VGSEnable()
+" }}}
+" vim_gnome_shell#Enable {{{
+function! vim_gnome_shell#Enable()
     python VGSEnable()
     python VGSUpdateState()
 endfunction
-
-function! vim_gnome_shell#VGSDisable()
+" }}}
+" vim_gnome_shell#Disable {{{
+function! vim_gnome_shell#Disable()
     python VGSDisable()
     python VGSUpdateState()
 endfunction
-
-function! vim_gnome_shell#VGSInsert(...)
+" }}}
+" vim_gnome_shell#Insert {{{
+function! vim_gnome_shell#Insert(...)
     python insert(vim.eval("a:000"))
 endfunction
-
-function! vim_gnome_shell#VGSAppend(...)
+" }}}
+" vim_gnome_shell#Append {{{
+function! vim_gnome_shell#Append(...)
     python append(vim.eval("a:000"))
 endfunction
-
-function! vim_gnome_shell#VGS()
-    if(g:vim_gnome_shell_loaded)
-        python initVGS()
+" }}}
+" vim_gnome_shell#Show {{{
+function! vim_gnome_shell#Show ()
+    let vgswinnr = bufwinnr('__VGS__')
+    if vgswinnr != -1
+        if winnr() != vgswinnr
+            call s:goto_win(vsgwinnr)
+        endif
+        return
+    endif
+    if g:vim_gnome_shell_vertical == 0
+        let openpos = 'topleft vertical'
+        let width = g:vim_gnome_shell_width
+    else
+        let openpos = 'leftabove'
+        let width = g:vim_gnome_shell_vertical
+    endif
+    exe 'silent keepalt ' . openpos . width . ' split ' . '__VGS__'
+    setlocal noreadonly
+    setlocal buftype=nofile
+    setlocal bufhidden=hide
+    setlocal noswapfile
+    setlocal nobuflisted
+    setlocal nomodifiable
+    setlocal nolist
+    setlocal nowrap
+    setlocal winfixwidth
+    setlocal textwidth=0
+    setlocal nospell
+    setlocal nonumber
+    setlocal norelativenumber
+endfunction
+" }}}
+" vim_gnome_shell#Init() {{{
+function! vim_gnome_shell#Init()
+    python initVGS()
+    if(g:vim_gnome_shell_loaded == 1)
         python VGSUpdateState()
-        let g:vim_gnome_shell_cwd = s:GetCWD()
+
         call s:SetCWD()
         echo "CWD:" g:vim_gnome_shell_cwd
         echo "UUID:" g:vim_gnome_shell_uuid
         echo "Name:" g:vim_gnome_shell_name
-        echo "State:" g:vim_gnome_shell_state
+        echo "State:" g:vim_gnome_shell_state ? 'Enabled' : 'Disabled'
     else
         return
     endif
 endfunction
+" }}}
